c7cf58ba8ae3061ac17a4977636ca1bff8963bc5
Juan Zava <jmzava@gmail.com>
2021-12-20 10:04:13 -0300

    desafio 2

---
+++ buceoecom/src/components/NavBar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+
+function NavBar() {
+  return (
+    <div>
+      
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home"> Tienda Buceo </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home"> Home </Nav.Link>
+            <Nav.Link href="#shop"> Shop </Nav.Link>
+            <Nav.Link href="#login"> Log In </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
+
+// import React from "react";
+// import Navbar from "react-bootstrap/Navbar";
+// import Nav from "react-bootstrap/Nav";
+//
+
+// function NavBar() {
+//     return (
+//     <Navbar bg="primary" variant="dark"></Navbar>
+//     )
+// }
+// export default NavBar;
+
+// import React from "react"; // usar rfce
+
+// import Navbar from "react-bootstrap/Navbar";
+//
+//
+
+// const NavBar = () => {
+//     return (
+
+//   );
+// };
+
+// export default NavBar;
